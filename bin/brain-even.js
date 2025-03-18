01fc4595559ab4749f2e63382c89e9ba193864eb
kawaiirazoru <felixxgg42@gmail.com>
2025-03-18 17:05:58 +0000

    fifth step

---
+++ bin/brain-even.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+
+import { greetings, askQuestion, checkAnswer } from '../src/cli.js';
+import { getRandomNumber } from '../src/utils.js';
+
+const isEven = (number) => number % 2 === 0;
+
+const playEvenGame = () => {
+  const name = greetings();
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+  let correctAnswersCount = 0;
+
+  while (correctAnswersCount < 3) {
+    const number = getRandomNumber(1, 100);
+    console.log(`Question: ${number}`);
+    const userAnswer = askQuestion('Your answer: ');
+    const correctAnswer = isEven(number) ? 'yes' : 'no';
+
+    if (userAnswer === correctAnswer) {
+      console.log('Correct!');
+      correctAnswersCount += 1;
+    } else {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      console.log(`Let's try again, ${name}!`);
+      return;
+    }
+  }
+
+  console.log(`Congratulations, ${name}!`);
+};
+
+playEvenGame();
